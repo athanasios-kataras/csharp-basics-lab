c7387ad2ee06209598a84ff5ceaceceaf6ce356b
Athanasios Kataras <athanasios.kataras@hotmail.com>
2020-09-18 19:48:47 +0300

    Adding the 1st day labs to the project

---
+++ Microsoft.CSharp.Basics/OOPFundamentals/CleanCode/Microsoft.CSharp.Basics.CleanCode/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.CSharp.Basics.CleanCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
