987d0962db5268685c1d48026da00b5bd59e1d25
Athanasios Kataras <athanasios.kataras@hotmail.com>
2020-09-21 15:31:09 +0300

    Adding skeleton folders for the GoF creational patterns

---
+++ Microsoft.CSharp.Basics/OOPPatterns/Creational/BankingProducts/Microsoft.CSharp.Basics.Patterns.BankingProducts/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Microsoft.CSharp.Basics.Patterns.BankingProducts
+{
+    public class Class1
+    {
+    }
+}
---
+++ Microsoft.CSharp.Basics/OOPPatterns/Creational/SingletonCache/Microsoft.CSharp.Basics.Patterns.SingletonCache/MemoryCache.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Microsoft.CSharp.Basics.Patterns.SingletonCache
+{
+    /// <summary>
+    /// Thread Safe Singleton Cache Class
+    /// </summary>
+    public sealed class Cache
+    {
+        private Cache cache;
+
+        private Cache() {
+            
+        }
+
+        public Cache GetInstance() {
+            if (cache == null) {
+                cache = new Cache();
+            }
+            return cache;
+        }
+        Dictionary<string, object> _cache = new Dictionary<string, object>();
+
+        public object GetOrCreate(string key, object createItem)
+        {
+            if (!_cache.ContainsKey(key))
+            {
+                _cache[key] = createItem;
+            }
+            return _cache[key];
+        }
+    }
+}
