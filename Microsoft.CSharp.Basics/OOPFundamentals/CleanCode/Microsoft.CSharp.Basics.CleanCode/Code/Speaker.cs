6e852211ff5687a67fcb0d720424cde3a1dc1d4d
ΝΤΑΣΟΠΟΥΛΟΣ ΣΤΕΛΙΟΣ <stelios.dassopoulos@alpha.gr>
2020-09-22 16:42:43 +0300

    Refactoring

--- Microsoft.CSharp.Basics/OOPFundamentals/CleanCode/Microsoft.CSharp.Basics.CleanCode/Code/Speaker.cs
+++ Microsoft.CSharp.Basics/OOPFundamentals/CleanCode/Microsoft.CSharp.Basics.CleanCode/Code/Speaker.cs
@@ -28,119 +28,113 @@ namespace Microsoft.CSharp.Basics.CleanCode
         public RegisterResponse Register(IRepository repository)
         {
             int? speakerId = null;
-            bool goodSpeaker = false;
 
-            if (!string.IsNullOrWhiteSpace(FirstName))
-            {
-                if (!string.IsNullOrWhiteSpace(LastName))
-                {
-                    if (!string.IsNullOrWhiteSpace(Email))
-                    {
-                        var employers = new List<string>() { "Pluralsight", "Microsoft", "Google" };
-
-                        goodSpeaker = Experience > 10 || HasBlog || Certifications.Count() > 3 || employers.Contains(Employer);
-
-                        if (!goodSpeaker)
-                        {
-                            //need to get just the domain from the email
-                            string emailDomain = Email.Split('@').Last();
-                            var domains = new List<string>() { "aol.com", "prodigy.com", "compuserve.com" };
-                            if (!domains.Contains(emailDomain) && (!(Browser.Name == WebBrowser.BrowserName.InternetExplorer && Browser.MajorVersion < 9)))
-                            {
-                                goodSpeaker = true;
-                            }
-                        }
-
-                        if (goodSpeaker)
-                        {
-                            bool approved = false;
-
-                            if (Sessions.Count() != 0)
-                            {
-                                foreach (var session in Sessions)
-                                {
-                                    var oldTech = new List<string>() { "Cobol", "Punch Cards", "Commodore", "VBScript" };
-                                    foreach (var tech in oldTech)
-                                    {
-                                        if (session.Title.Contains(tech) || session.Description.Contains(tech))
-                                        {
-                                            session.Approved = false;
-                                            break;
-                                        }
-                                        else
-                                        {
-                                            session.Approved = true;
-                                            approved = true;
-                                        }
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                return new RegisterResponse(RegisterError.NoSessionsProvided);
-                            }
-
-                            if (approved)
-                            {
-                                //if we got this far, the speaker is approved
-                                //let's go ahead and register him/her now.
-                                //First, let's calculate the registration fee. 
-                                //More experienced speakers pay a lower fee.
-                                if (Experience <= 1)
-                                {
-                                    RegistrationFee = 500;
-                                }
-                                else if (Experience >= 2 && Experience <= 3)
-                                {
-                                    RegistrationFee = 250;
-                                }
-                                else if (Experience >= 4 && Experience <= 5)
-                                {
-                                    RegistrationFee = 100;
-                                }
-                                else if (Experience >= 6 && Experience <= 9)
-                                {
-                                    RegistrationFee = 50;
-                                }
-                                else
-                                {
-                                    RegistrationFee = 0;
-                                }
-
-                                try
-                                {
-                                    speakerId = repository.SaveSpeaker(this);
-                                }
-                                catch
-                                {
-                                }
-                            }
-                            else
-                            {
-                                return new RegisterResponse(RegisterError.NoSessionsApproved);
-                            }
-                        }
-                        else
-                        {
-                            return new RegisterResponse(RegisterError.SpeakerDoesNotMeetStandards);
-                        }
-                    }
-                    else
-                    {
-                        return new RegisterResponse(RegisterError.EmailRequired);
-                    }
-                }
-                else
-                {
-                    return new RegisterResponse(RegisterError.LastNameRequired);
-                }
-            }
-            else
+            if (string.IsNullOrWhiteSpace(FirstName))
             {
                 return new RegisterResponse(RegisterError.FirstNameRequired);
             }
 
+            if (string.IsNullOrWhiteSpace(LastName))
+            {
+                return new RegisterResponse(RegisterError.LastNameRequired);
+            }
+
+            if (string.IsNullOrWhiteSpace(Email))
+            {
+                return new RegisterResponse(RegisterError.EmailRequired);
+            }
+
+            if (!IsGoodSpeaker())
+            {
+                return new RegisterResponse(RegisterError.SpeakerDoesNotMeetStandards);
+            }
+
+            if (Sessions.Count() == 0)
+            {
+                return new RegisterResponse(RegisterError.NoSessionsProvided);
+            }
+
+            if (!isApprovedSpeaker())
+            {
+                return new RegisterResponse(RegisterError.NoSessionsApproved);
+            }
+
+            CalculateRegistrationFee();
+
+            try
+            {
+                speakerId = repository.SaveSpeaker(this);
+            }
+            catch
+            {
+
+            }
+
             return new RegisterResponse((int)speakerId);
+        }
+
+        private bool IsGoodSpeaker()
+        {
+            var employers = new List<string>() { "Pluralsight", "Microsoft", "Google" };
+            bool isGoodSpeaker = Experience > 10 || HasBlog || Certifications.Count() > 3 || employers.Contains(Employer);
+
+            if (!isGoodSpeaker)
+            {
+                string emailDomain = Email.Split('@').Last();
+                var domains = new List<string>() { "aol.com", "prodigy.com", "compuserve.com" };
+                isGoodSpeaker = domains.Contains(emailDomain) || (Browser.Name == WebBrowser.BrowserName.InternetExplorer && Browser.MajorVersion < 9);
+            }
+
+            return isGoodSpeaker;
+        }
+
+        private bool isApprovedSpeaker()
+        {
+            bool isApproved = false;
+
+            foreach (var session in Sessions)
+            {
+                var oldTech = new List<string>() { "Cobol", "Punch Cards", "Commodore", "VBScript" };
+                foreach (var tech in oldTech)
+                {
+                    if (session.Title.Contains(tech) || session.Description.Contains(tech))
+                    {
+                        session.Approved = false;
+                        break;
+                    }
+                    else
+                    {
+                        session.Approved = true;
+                        isApproved = true;
+                    }
+                }
+            }
+
+            return isApproved;
+        }
+
+        private void CalculateRegistrationFee()
+        {
+            if (Experience <= 1)
+            {
+                RegistrationFee = 500;
+            }
+            else if (Experience >= 2 && Experience <= 3)
+            {
+                RegistrationFee = 250;
+            }
+            else if (Experience >= 4 && Experience <= 5)
+            {
+                RegistrationFee = 100;
+            }
+            else if (Experience >= 6 && Experience <= 9)
+            {
+                RegistrationFee = 50;
+            }
+            else
+            {
+                RegistrationFee = 0;
+            }
         }
     }
 }
