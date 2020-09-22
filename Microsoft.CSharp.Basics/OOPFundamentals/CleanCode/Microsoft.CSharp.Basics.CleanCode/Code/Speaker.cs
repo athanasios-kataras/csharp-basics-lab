902dc1cb327790a8826ac77a9293eb1f0d980f44
ppanakos <paraskevas.panakos@alpha.gr>
2020-09-22 16:40:10 +0300

    Fix3

--- Microsoft.CSharp.Basics/OOPFundamentals/CleanCode/Microsoft.CSharp.Basics.CleanCode/Code/Speaker.cs
+++ Microsoft.CSharp.Basics/OOPFundamentals/CleanCode/Microsoft.CSharp.Basics.CleanCode/Code/Speaker.cs
@@ -27,117 +27,96 @@ namespace Microsoft.CSharp.Basics.CleanCode
 
 		public RegisterResponse Register(IRepository repository)
 		{
-			if (!string.IsNullOrWhiteSpace(FirstName))
-			{
-				if (!string.IsNullOrWhiteSpace(LastName))
-				{
-					if (!string.IsNullOrWhiteSpace(Email))
-					{
-
-						int? speakerId = null;
-						bool isHighlyExperienced = false;
-						bool hasApprovedSessions = false;
-						var technologies = new List<string>() { "Cobol", "Punch Cards", "Commodore", "VBScript" };
-						var emailDomains = new List<string>() { "aol.com", "prodigy.com", "compuserve.com" };
-						var employers = new List<string>() { "Pluralsight", "Microsoft", "Google" };
-
-						isHighlyExperienced = ExperienceLevel > 10 || HasBlog || Certifications.Count() > 3 || employers.Contains(Employer);
-
-						if (!isHighlyExperienced)
-						{
-							
-							string emailDomain = Email.Split('@').Last();
-
-							if (!emailDomains.Contains(emailDomain) && (!(Browser.Name == WebBrowser.BrowserName.InternetExplorer && Browser.MajorVersion < 9)))
-							{
-								isHighlyExperienced = true;
-							}
-						}
-
-						if (isHighlyExperienced)
-						{
-							if (Sessions.Count() != 0)
-							{
-								foreach (var session in Sessions)
-								{
-									foreach (var tech in technologies)
-									{
-										if (session.Title.Contains(tech) || session.Description.Contains(tech))
-										{
-											session.Approved = false;
-											break;
-										}
-										else
-										{
-											session.Approved = true;
-											hasApprovedSessions = true;
-										}
-									}
-								}
-							}
-							else
-							{
-								return new RegisterResponse(RegisterError.NoSessionsProvided);
-							}
-
-							if (hasApprovedSessions)
-							{
-
-								if (ExperienceLevel <= 1)
-								{
-									RegistrationFee = 500;
-								}
-								else if (ExperienceLevel >= 2 && ExperienceLevel <= 3)
-								{
-									RegistrationFee = 250;
-								}
-								else if (ExperienceLevel >= 4 && ExperienceLevel <= 5)
-								{
-									RegistrationFee = 100;
-								}
-								else if (ExperienceLevel >= 6 && ExperienceLevel <= 9)
-								{
-									RegistrationFee = 50;
-								}
-								else
-								{
-									RegistrationFee = 0;
-								}
-
-								try
-								{
-									speakerId = repository.SaveSpeaker(this);
-								}
-								catch
-								{
-
-								}
-							}
-							else
-							{
-								return new RegisterResponse(RegisterError.NoSessionsApproved);
-							}
-						}
-						else
-						{
-							return new RegisterResponse(RegisterError.SpeakerDoesNotMeetStandards);
-						}
-					}
-					else
-					{
-						return new RegisterResponse(RegisterError.EmailRequired);
-					}
-				}
-				else
-				{
-					return new RegisterResponse(RegisterError.LastNameRequired);
-				}
-			}
-			else
+			
+			if (string.IsNullOrWhiteSpace(FirstName))
 			{
 				return new RegisterResponse(RegisterError.FirstNameRequired);
 			}
+			if (string.IsNullOrWhiteSpace(LastName))
+			{
+				return new RegisterResponse(RegisterError.LastNameRequired);
+			}
+			if (!string.IsNullOrWhiteSpace(Email))
+			{
+				return new RegisterResponse(RegisterError.EmailRequired);
+            }
 
+			int? speakerId = null;
+            bool isHighlyExperienced = false;
+            bool hasApprovedSessions = false;
+			var technologies = new List<string>() { "Cobol", "Punch Cards", "Commodore", "VBScript" };
+			var emailDomains = new List<string>() { "aol.com", "prodigy.com", "compuserve.com" };
+
+			var employers = new List<string>() { "Pluralsight", "Microsoft", "Google" };
+
+			isHighlyExperienced = ExperienceLevel > 10 || HasBlog || Certifications.Count() > 3 || employers.Contains(Employer);
+
+			if (!isHighlyExperienced)
+			{		
+				string emailDomain = Email.Split('@').Last();
+
+                if (emailDomains.Contains(emailDomain) || Browser.Name == WebBrowser.BrowserName.InternetExplorer && Browser.MajorVersion < 9)
+                {
+                    return new RegisterResponse(RegisterError.SpeakerDoesNotMeetStandards);
+                }
+            }
+
+			if (Sessions.Count() == 0)
+			{
+				return new RegisterResponse(RegisterError.NoSessionsProvided);
+            }
+
+            foreach (var session in Sessions)
+			{
+				foreach (var tech in technologies)
+				{
+					if (session.Title.Contains(tech) || session.Description.Contains(tech))
+					{
+						session.Approved = false;
+						break;
+					}
+					else
+					{
+						session.Approved = true;
+						hasApprovedSessions = true;
+					}
+				}
+			}
+
+			if (!hasApprovedSessions)
+			{
+				return new RegisterResponse(RegisterError.NoSessionsApproved);
+			}			
+
+			if (ExperienceLevel <= 1)
+			{
+				RegistrationFee = 500;
+			}
+			else if (ExperienceLevel >= 2 && ExperienceLevel <= 3)
+			{
+				RegistrationFee = 250;
+			}
+			else if (ExperienceLevel >= 4 && ExperienceLevel <= 5)
+			{
+				RegistrationFee = 100;
+			}
+			else if (ExperienceLevel >= 6 && ExperienceLevel <= 9)
+			{
+				RegistrationFee = 50;
+			}
+			else
+			{
+				RegistrationFee = 0;
+			}
+
+			try
+			{
+				speakerId = repository.SaveSpeaker(this);
+			}
+			catch
+			{
+
+			}
 			return new RegisterResponse((int)speakerId);
 		}
 	}
