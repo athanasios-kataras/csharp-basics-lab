15ac8ddb70194bd725ca4225fae5be4824e5ae28
ΝΤΑΣΟΠΟΥΛΟΣ ΣΤΕΛΙΟΣ <stelios.dassopoulos@alpha.gr>
2020-09-22 16:43:55 +0300

    Changes Exp in unit test

--- Microsoft.CSharp.Basics/OOPFundamentals/CleanCode/Microsoft.CSharp.Basics.CleanCode.UnitTests/Tests/SpeakerTests.cs
+++ Microsoft.CSharp.Basics/OOPFundamentals/CleanCode/Microsoft.CSharp.Basics.CleanCode.UnitTests/Tests/SpeakerTests.cs
@@ -3,187 +3,187 @@ using NUnit.Framework;
 
 namespace Microsoft.CSharp.Basics.CleanCode.UnitTests
 {
-	[TestFixture]
-	public class SpeakerTests
-	{
-		private FakeRepository repository = new FakeRepository();
+    [TestFixture]
+    public class SpeakerTests
+    {
+        private FakeRepository repository = new FakeRepository();
 
-		[Test]
-		public void Register_EmptyFirstName_ReturnsFirstNameRequired()
-		{
-			//arrange
-			var speaker = GetSpeakerThatWouldBeApproved();
-			speaker.FirstName = "";
+        [Test]
+        public void Register_EmptyFirstName_ReturnsFirstNameRequired()
+        {
+            //arrange
+            var speaker = GetSpeakerThatWouldBeApproved();
+            speaker.FirstName = "";
 
-			//act
-			var result = speaker.Register(repository);
+            //act
+            var result = speaker.Register(repository);
 
-			//assert
-			Assert.AreEqual(RegisterError.FirstNameRequired, result.Error);
-		}
+            //assert
+            Assert.AreEqual(RegisterError.FirstNameRequired, result.Error);
+        }
 
-		[Test]
-		public void Register_EmptyLastName_ReturnsLastNameRequired()
-		{
-			//arrange
-			var speaker = GetSpeakerThatWouldBeApproved();
-			speaker.LastName = "";
+        [Test]
+        public void Register_EmptyLastName_ReturnsLastNameRequired()
+        {
+            //arrange
+            var speaker = GetSpeakerThatWouldBeApproved();
+            speaker.LastName = "";
 
-			//act
-			var result = speaker.Register(repository);
+            //act
+            var result = speaker.Register(repository);
 
-			//assert
-			Assert.AreEqual(RegisterError.LastNameRequired, result.Error);
-		}
+            //assert
+            Assert.AreEqual(RegisterError.LastNameRequired, result.Error);
+        }
 
-		[Test]
-		public void Register_EmptyEmail_ReturnsEmailRequired()
-		{
-			//arrange
-			var speaker = GetSpeakerThatWouldBeApproved();
-			speaker.Email = "";
+        [Test]
+        public void Register_EmptyEmail_ReturnsEmailRequired()
+        {
+            //arrange
+            var speaker = GetSpeakerThatWouldBeApproved();
+            speaker.Email = "";
 
-			//act
-			var result = speaker.Register(repository);
+            //act
+            var result = speaker.Register(repository);
 
-			//assert
-			Assert.AreEqual(RegisterError.EmailRequired, result.Error);
-		}
+            //assert
+            Assert.AreEqual(RegisterError.EmailRequired, result.Error);
+        }
 
-		[Test]
-		public void Register_WorksForPrestigiousEmployerButHasRedFlags_ReturnsSpeakerId()
-		{
-			//arrange
-			var speaker = GetSpeakerWithRedFlags();
-			speaker.Employer = "Microsoft";
+        [Test]
+        public void Register_WorksForPrestigiousEmployerButHasRedFlags_ReturnsSpeakerId()
+        {
+            //arrange
+            var speaker = GetSpeakerWithRedFlags();
+            speaker.Employer = "Microsoft";
 
-			//act
-			var result = speaker.Register(new FakeRepository());
+            //act
+            var result = speaker.Register(new FakeRepository());
 
-			//assert
-			Assert.IsNotNull(result.SpeakerId);
-		}
+            //assert
+            Assert.IsNotNull(result.SpeakerId);
+        }
 
-		[Test]
-		public void Register_HasBlogButHasRedFlags_ReturnsSpeakerId()
-		{
-			//arrange
-			var speaker = GetSpeakerWithRedFlags();
+        [Test]
+        public void Register_HasBlogButHasRedFlags_ReturnsSpeakerId()
+        {
+            //arrange
+            var speaker = GetSpeakerWithRedFlags();
 
-			//act
-			var result = speaker.Register(new FakeRepository());
+            //act
+            var result = speaker.Register(new FakeRepository());
 
-			//assert
-			Assert.IsNotNull(result.SpeakerId);
-		}
+            //assert
+            Assert.IsNotNull(result.SpeakerId);
+        }
 
-		[Test]
-		public void Register_HasCertificationsButHasRedFlags_ReturnsSpeakerId()
-		{
-			//arrange
-			var speaker = GetSpeakerWithRedFlags();
-			speaker.Certifications = new List<string>()
-		{
-			"cert1",
-			"cert2",
-			"cert3",
-			"cert4"
-		};
+        [Test]
+        public void Register_HasCertificationsButHasRedFlags_ReturnsSpeakerId()
+        {
+            //arrange
+            var speaker = GetSpeakerWithRedFlags();
+            speaker.Certifications = new List<string>()
+        {
+            "cert1",
+            "cert2",
+            "cert3",
+            "cert4"
+        };
 
-			//act
-			var result = speaker.Register(new FakeRepository());
+            //act
+            var result = speaker.Register(new FakeRepository());
 
-			//assert
-			Assert.IsNotNull(result.SpeakerId);
-		}
+            //assert
+            Assert.IsNotNull(result.SpeakerId);
+        }
 
-		[Test]
-		public void Register_SingleSessionThatsOnOldTech_ReturnsNoSessionsApproved()
-		{
-			//arrange
-			var speaker = GetSpeakerThatWouldBeApproved();
-			speaker.Sessions = new List<Session>() {
-			new Session("Cobol for dummies", "Intro to Cobol")
-		};
+        [Test]
+        public void Register_SingleSessionThatsOnOldTech_ReturnsNoSessionsApproved()
+        {
+            //arrange
+            var speaker = GetSpeakerThatWouldBeApproved();
+            speaker.Sessions = new List<Session>() {
+            new Session("Cobol for dummies", "Intro to Cobol")
+        };
 
-			//act
-			var result = speaker.Register(repository);
+            //act
+            var result = speaker.Register(repository);
 
-			//assert
-			Assert.AreEqual(RegisterError.NoSessionsApproved, result.Error);
-		}
+            //assert
+            Assert.AreEqual(RegisterError.NoSessionsApproved, result.Error);
+        }
 
-		[Test]
-		public void Register_NoSessionsPassed_ReturnsNoSessionsProvidedError()
-		{
-			//arrange
-			var speaker = GetSpeakerThatWouldBeApproved();
-			speaker.Sessions = new List<Session>();
+        [Test]
+        public void Register_NoSessionsPassed_ReturnsNoSessionsProvidedError()
+        {
+            //arrange
+            var speaker = GetSpeakerThatWouldBeApproved();
+            speaker.Sessions = new List<Session>();
 
-			//act
-			var result = speaker.Register(repository);
+            //act
+            var result = speaker.Register(repository);
 
-			//assert
-			Assert.AreEqual(RegisterError.NoSessionsProvided, result.Error);
-		}
+            //assert
+            Assert.AreEqual(RegisterError.NoSessionsProvided, result.Error);
+        }
 
-		[Test]
-		public void Register_DoesntAppearExceptionalAndUsingOldBrowser_ReturnsSpeakerDoesNotMeetStandards()
-		{
-			//arrange
-			var speakerThatDoesntAppearExceptional = GetSpeakerThatWouldBeApproved();
-			speakerThatDoesntAppearExceptional.HasBlog = false;
-			speakerThatDoesntAppearExceptional.Browser = new WebBrowser("IE", 6);
+        [Test]
+        public void Register_DoesntAppearExceptionalAndUsingOldBrowser_ReturnsSpeakerDoesNotMeetStandards()
+        {
+            //arrange
+            var speakerThatDoesntAppearExceptional = GetSpeakerThatWouldBeApproved();
+            speakerThatDoesntAppearExceptional.HasBlog = false;
+            speakerThatDoesntAppearExceptional.Browser = new WebBrowser("IE", 6);
 
-			//act
-			var result = speakerThatDoesntAppearExceptional.Register(repository);
+            //act
+            var result = speakerThatDoesntAppearExceptional.Register(repository);
 
-			//assert
-			Assert.AreEqual(RegisterError.SpeakerDoesNotMeetStandards, result.Error);
-		}
+            //assert
+            Assert.AreEqual(RegisterError.SpeakerDoesNotMeetStandards, result.Error);
+        }
 
-		[Test]
-		public void Register_DoesntAppearExceptionalAndHasAncientEmail_ReturnsSpeakerDoesNotMeetStandards()
-		{
-			//arrange
-			var speakerThatDoesntAppearExceptional = GetSpeakerThatWouldBeApproved();
-			speakerThatDoesntAppearExceptional.HasBlog = false;
-			speakerThatDoesntAppearExceptional.Email = "name@aol.com";
+        [Test]
+        public void Register_DoesntAppearExceptionalAndHasAncientEmail_ReturnsSpeakerDoesNotMeetStandards()
+        {
+            //arrange
+            var speakerThatDoesntAppearExceptional = GetSpeakerThatWouldBeApproved();
+            speakerThatDoesntAppearExceptional.HasBlog = false;
+            speakerThatDoesntAppearExceptional.Email = "name@aol.com";
 
-			//act
-			var result = speakerThatDoesntAppearExceptional.Register(repository);
+            //act
+            var result = speakerThatDoesntAppearExceptional.Register(repository);
 
-			//assert
-			Assert.AreEqual(RegisterError.SpeakerDoesNotMeetStandards, result.Error);
-		}
+            //assert
+            Assert.AreEqual(RegisterError.SpeakerDoesNotMeetStandards, result.Error);
+        }
 
-		#region Helpers
-		private Speaker GetSpeakerThatWouldBeApproved()
-		{
-			return new Speaker()
-			{
-				FirstName = "First",
-				LastName = "Last",
-				Email = "example@domain.com",
-				Employer = "Example Employer",
-				HasBlog = true,
-				Browser = new WebBrowser("test", 1),
-				Exp = 1,
-				Certifications = new System.Collections.Generic.List<string>(),
-				BlogURL = "",
-				Sessions = new System.Collections.Generic.List<Session>() {
-				new Session("test title", "test description")
-			}
-			};
-		}
+        #region Helpers
+        private Speaker GetSpeakerThatWouldBeApproved()
+        {
+            return new Speaker()
+            {
+                FirstName = "First",
+                LastName = "Last",
+                Email = "example@domain.com",
+                Employer = "Example Employer",
+                HasBlog = true,
+                Browser = new WebBrowser("test", 1),
+                Experience = 1,
+                Certifications = new System.Collections.Generic.List<string>(),
+                BlogURL = "",
+                Sessions = new System.Collections.Generic.List<Session>() {
+                new Session("test title", "test description")
+            }
+            };
+        }
 
-		private Speaker GetSpeakerWithRedFlags()
-		{
-			var speaker = GetSpeakerThatWouldBeApproved();
-			speaker.Email = "tom@aol.com";
-			speaker.Browser = new WebBrowser("IE", 6);
-			return speaker;
-		}
-		#endregion
-	}
+        private Speaker GetSpeakerWithRedFlags()
+        {
+            var speaker = GetSpeakerThatWouldBeApproved();
+            speaker.Email = "tom@aol.com";
+            speaker.Browser = new WebBrowser("IE", 6);
+            return speaker;
+        }
+        #endregion
+    }
 }
