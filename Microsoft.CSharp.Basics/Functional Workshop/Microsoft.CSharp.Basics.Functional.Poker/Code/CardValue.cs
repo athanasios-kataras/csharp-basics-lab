f827289ffbcd284d19c19602177d3facf2437eaa
Athanasios Kataras <athanasios.kataras@hotmail.com>
2020-09-22 15:34:50 +0300

    Remporary comment

--- Microsoft.CSharp.Basics/Functional Workshop/Microsoft.CSharp.Basics.Functional.Poker/Code/CardValue.cs
+++ Microsoft.CSharp.Basics/Functional Workshop/Microsoft.CSharp.Basics.Functional.Poker/Code/CardValue.cs
@@ -1,7 +1,4 @@
 namespace Microsoft.CSharp.Basics.Functional.Poker.Code
 {
-    public class CardValue
-    {
         public enum CardValue { Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace }
-    }
 }
--- Microsoft.CSharp.Basics/Functional Workshop/Microsoft.CSharp.Basics.Functional.Poker/Code/Hand.cs
+++ Microsoft.CSharp.Basics/Functional Workshop/Microsoft.CSharp.Basics.Functional.Poker/Code/Hand.cs
@@ -1,56 +1,58 @@
-namespace Microsoft.CSharp.Basics.Functional.Poker.Code
-{
-    public class Hand
-    {
-        // private readonly setter for cards, initilaizes a new List<Card>
-        private readonly List<Card> cards = new List<Card>();
+// using System.Collections.Generic;
+// using System.Linq;
+// namespace Microsoft.CSharp.Basics.Functional.Poker.Code
+// {
+//     public class Hand
+//     {
+//         // private readonly setter for cards, initilaizes a new List<Card>
+//         private readonly List<Card> cards = new List<Card>();
 
-        // To limit mutablility of the Cards property, an IEnumerable is returned instead of List
-        // Cards is mutable but only by calling Draw
-        public IEnumerable<Card> Cards { get { return cards; } }
+//         // To limit mutablility of the Cards property, an IEnumerable is returned instead of List
+//         // Cards is mutable but only by calling Draw
+//         public IEnumerable<Card> Cards { get { return cards; } }
 
-        public void Draw(Card card)
-        {
-            cards.Add(card);
-        }
+//         public void Draw(Card card)
+//         {
+//             cards.Add(card);
+//         }
 
-        // A LINQ Aggregate is used to find the HighCard
+//         // A LINQ Aggregate is used to find the HighCard
 
-        public Card HighCard()
-        {
-            // Card highCard = Cards.First();
-            // foreach (var nextCard in Cards)
-            // {
-            //     if (nextCard.Value > highCard.Value)
-            //     {
-            //         highCard = nextCard;
-            //     }
-            // }
-            // return highCard;
-            return cards.Aggregate((highCard, nextCard) => nextCard.Value > highCard.Value ? nextCard : highCard);
-            // OrderBy is also valid, but could use more resources than Aggregate
-            //return cards.OrderBy(c => c.Value).Last();
-        }
+//         public Card HighCard()
+//         {
+//             // Card highCard = Cards.First();
+//             // foreach (var nextCard in Cards)
+//             // {
+//             //     if (nextCard.Value > highCard.Value)
+//             //     {
+//             //         highCard = nextCard;
+//             //     }
+//             // }
+//             // return highCard;
+//             return cards.Aggregate((highCard, nextCard) => nextCard.Value > highCard.Value ? nextCard : highCard);
+//             // OrderBy is also valid, but could use more resources than Aggregate
+//             //return cards.OrderBy(c => c.Value).Last();
+//         }
         
-        // A Return Early pattern is used to call each hand ranking function
-        public HandRank GetHandRank()
-        {
-            if (HasRoyalFlush()) return HandRank.RoyalFlush;
-            if (HasFlush()) return HandRank.Flush;
-            return HandRank.HighCard;
-        }
+//         // A Return Early pattern is used to call each hand ranking function
+//         // public HandRank GetHandRank()
+//         // {
+//         //     if (HasRoyalFlush()) return HandRank.RoyalFlush;
+//         //     if (HasFlush()) return HandRank.Flush;
+//         //     return HandRank.HighCard;
+//         // }
 
-        // A LINQ All method combined with First can check if all suits are the same value
-        private bool HasFlush()
-        {
-            return cards.All(c => cards.First().Suit == c.Suit);
-        }
+//         // A LINQ All method combined with First can check if all suits are the same value
+//         private bool HasFlush()
+//         {
+//             return cards.All(c => cards.First().Suit == c.Suit);
+//         }
 
-        // A LINQ All method can determine if all cards are greater than Nine or [Ten, Jack, Queen, King, Ace ]
-        public bool HasRoyalFlush()
-        {
-            return HasFlush() && cards.All(c => c.Value > CardValue.Nine);
-        }
+//         // A LINQ All method can determine if all cards are greater than Nine or [Ten, Jack, Queen, King, Ace ]
+//         public bool HasRoyalFlush()
+//         {
+//             return HasFlush() && cards.All(c => c.Value > CardValue.Nine);
+//         }
 
-    }
-}
+//     }
+// }
